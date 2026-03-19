c8132fa7f1afce51a922e58d387943c6c1bf6e4e
redsaeok <74396365+redsaeok@users.noreply.github.com>
2026-03-19 10:22:52 -0700

    Updated joke card component to more closely match the example in prep for cliciking elements

--- src/components/JokeCard.tsx
+++ src/components/JokeCard.tsx
@@ -9,13 +9,20 @@ type TJokeCardProps = {
 
 export default function JokeCard( { jokeCardProp }: TJokeCardProps) {
     return (
-        <div className="shadow-md rounded-md p-6 w-sm max-w-sm mx-auto mt-20">
-            <h3>{jokeCardProp.setup}</h3>
-            <p>{jokeCardProp.punchline}</p>
-            <span>
-                Likes: {jokeCardProp.likeCount} 👍 or 👎.
-            </span>
-            <span>🗑️ Delete</span>
+        <div className="flex flex-row shadow-md rounded-md p-6 w-sm max-w-sm xl:w-xl xl: max-w-xl mx-auto mt-5">
+            <div className="flex flex-row rounded-sm shadow-sm">
+                <div className="rounded-md flex flex-col justify-evenly px-1">
+                    <span className="text-center">👍</span>
+                    <span className="text-center">{jokeCardProp.likeCount}</span>
+                    <span className="text-center">👎</span>
+                </div>
+
+            </div>
+            <div className="flex flex-col gap-2 mt-2 ml-5">
+                <h3 className="font-bold">{jokeCardProp.setup}</h3>
+                <p>{jokeCardProp.punchline}</p>        
+                <span>🗑️ Delete</span>                
+            </div>
         </div>
     )
 }
